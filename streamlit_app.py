7d4fc644b3ad154576085d30d03d26e5aa0f048f
jkkk <delrosso.cp@gmail.com>
2026-06-25 16:00:20 +0100

    Update streamlit_app.py
    corrected search_on. imported pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,9 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+
+pd_df = my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
 
@@ -29,8 +31,8 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
-        search_on = """ select search_on from smoothies.public.fruit_options where fruit_name = '"""+ fruit_chosen +"""'"""
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + session.sql(search_on).collect())  
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(INGREDIENTS, NAME_ON_ORDER)
